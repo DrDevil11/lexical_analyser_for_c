c6ddb5ff3c38aca307f65b7e657eda315118b89b
DrDevil11 <59121433+DrDevil11@users.noreply.github.com>
2021-02-28 12:34:33 +0530

    Add files via upload

---
+++ samplecode.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+
+int main()
+{ int i=0;
+  int a[5];
+  for(int i=0;i<10;i++)
+  {if(i>4)
+	{printf("hello world");}
+   else
+    {printf("mera pura life barbaad ho gaya");}
+  }
+
+//break;
+/*car*/
+//time
+return 1;
+}
